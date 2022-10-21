4c4bf6b042f10b7be171cd158b91f72d6c1ed267
b.chau <brianchau7979@gmail.com>
2022-10-21 14:43:04 -0500

    defining error template

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.get('/campgrounds/new', (req, res) => {
 })
 
 app.post('/campgrounds', catchAsync(async (req, res, next) => {
-  if(!req.body.campground) throw new ExpressError(' Invalid Campground Data', 400);
+  if (!req.body.campground) throw new ExpressError(' Invalid Campground Data', 400);
   const campground = new Campground(req.body.campground);
   await campground.save();
   res.redirect(`/campgrounds/${campground.id}`)
@@ -74,8 +74,9 @@ app.all('*', (req, res, next) => {
 })
 
 app.use((err, req, res, next) => {
-  const { statusCode = 500, message = 'Something went wrong' } = err;
-  res.status(statusCode).send(message);
+  const { statusCode = 500 } = err;
+  if (!err.message) err.message = 'Oh No, Something Went Wrong!';
+  res.status(statusCode).render('error', { err });
 })
 
 app.listen(3000, () => {
