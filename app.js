d4437f8f80396588a089f6c8d90f30a786486427
b.chau <brianchau7979@gmail.com>
2023-02-08 14:53:09 -0600

    Creating User Model

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const flash = require('connect-flash');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 
+
 const campgrounds = require('./routes/campgrounds');
 const reviews = require('./routes/reviews');
 
