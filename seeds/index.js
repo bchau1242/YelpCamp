dd8a1965ce046b0121e71addfae4c31d8f523e2c
b.chau <brianchau7979@gmail.com>
2023-02-26 10:28:36 -0600

    Fixing Our Seeds

--- seeds/index.js
+++ seeds/index.js
@@ -18,16 +18,25 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
   await Campground.deleteMany({});
-  for(let i = 0; i < 50; i++) {
+  for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       author: '63e5224516f69aca692ff7af',
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
-      image: 'http://source.unsplash.com/collection/484351',
       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure tempora harum eos illo eius, et veritatis provident deserunt ex error nihil! Quae, similique ipsum iste totam vitae temporibus earum minima!',
-      price 
+      price,
+      images: [
+        {
+          url: 'https://res.cloudinary.com/dd60somaj/image/upload/v1677269967/YelpCamp/vyfcigvqgj7gohqccfqx.avif',
+          filename: 'YelpCamp/vyfcigvqgj7gohqccfqx',
+        },
+        {
+          url: 'https://res.cloudinary.com/dd60somaj/image/upload/v1677269967/YelpCamp/isj9ri9d86u3wqbr9ju6.avif',
+          filename: 'YelpCamp/isj9ri9d86u3wqbr9ju6',
+        }
+      ]
     })
     await camp.save();
   }
