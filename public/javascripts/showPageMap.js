8fe5215d833105a49ea1c80a016678292e3f0c8b
b.chau <brianchau7979@gmail.com>
2023-03-01 09:49:16 -0600

    Customizing Map Popup

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -7,5 +7,11 @@ const map = new mapboxgl.Map({
 });
 
 new mapboxgl.Marker()
-.setLngLat(campground.geometry.coordinates)
-.addTo(map)
+  .setLngLat(campground.geometry.coordinates)
+  .setPopup(
+    new mapboxgl.Popup({ offset: 25 })
+      .setHTML(
+        `<h3>${campground.title}</h3><p>${campground.location}</p>`
+      )
+  )
+  .addTo(map)
