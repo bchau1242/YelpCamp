552cb28349210d83f877c92bb24caad8294c4155
b.chau <brianchau7979@gmail.com>
2023-03-02 17:16:53 -0600

    Minor Changes to Session/Cookies

--- app.js
+++ app.js
@@ -44,11 +44,13 @@ app.use(mongoSanitize({
 }));
 
 const sessionConfig = {
+  name: 'session',
   secret: 'thisshouldbeabettersecret!',
   resave: false,
   saveUninitialized: true,
   cookie: {
     httpOnly: true,
+    // secure: true,
     expire: Date.now() + 1000 * 60 * 60 * 24 * 7, 
     maxAge: 1000 * 60 * 60 * 24 * 7,
   }
