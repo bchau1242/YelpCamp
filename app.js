37ce0e6121c6199e0c2a1c192bc7e8e1d7b9a07d
b.chau <brianchau7979@gmail.com>
2023-03-03 12:37:10 -0600

    Using Mongo For Our Session Store

--- app.js
+++ app.js
@@ -15,14 +15,15 @@ const LocalStrategy = require("passport-local");
 const User = require("./models/user");
 const mongoSanitize = require("express-mongo-sanitize");
 const helmet = require("helmet");
-const dbUrl = process.env.DB_URL;
+const MongoDBStore = require("connect-mongo");
+// const dbUrl = process.env.DB_URL;
 
 const userRoutes = require("./routes/users");
 const campgroundRoutes = require("./routes/campgrounds");
 const reviewRoutes = require("./routes/reviews");
 
-// "mongodb://localhost:27017/yelp-camp"
-mongoose.connect("mongodb://localhost:27017/yelp-camp", {
+const dbUrl = "mongodb://localhost:27017/yelp-camp";
+mongoose.connect(dbUrl, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -44,13 +45,26 @@ app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(
   mongoSanitize({
-    replaceWith: "-",
+    replaceWith: "_",
   })
 );
 
+const secret = process.env.SECRET || "thisshouldbeabettersecret!";
+
+const store = MongoDBStore.create({
+  mongoUrl: dbUrl,
+  secret,
+  touchAfter: 24 * 60 * 60,
+});
+
+store.on("error", function (e) {
+  console.log("SESSION STORE ERROR", e);
+});
+
 const sessionConfig = {
+  store,
   name: "session",
-  secret: "thisshouldbeabettersecret!",
+  secret,
   resave: false,
   saveUninitialized: true,
   cookie: {
@@ -79,7 +93,7 @@ const styleSrcUrls = [
   "https://api.tiles.mapbox.com/",
   "https://fonts.googleapis.com/",
   "https://use.fontawesome.com/",
-  "https://cdn.jsdelivr.net"
+  "https://cdn.jsdelivr.net",
 ];
 const connectSrcUrls = [
   "https://api.mapbox.com/",
