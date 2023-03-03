fd5221bc6ec51a01d0501756d036bac213a398ee
b.chau <brianchau7979@gmail.com>
2023-03-03 16:14:58 -0600

    Ready to Deploy

--- app.js
+++ app.js
@@ -16,13 +16,13 @@ const User = require("./models/user");
 const mongoSanitize = require("express-mongo-sanitize");
 const helmet = require("helmet");
 const MongoDBStore = require("connect-mongo");
-// const dbUrl = process.env.DB_URL;
 
 const userRoutes = require("./routes/users");
 const campgroundRoutes = require("./routes/campgrounds");
 const reviewRoutes = require("./routes/reviews");
 
-const dbUrl = "mongodb://localhost:27017/yelp-camp";
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/yelp-camp";
+
 mongoose.connect(dbUrl, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
