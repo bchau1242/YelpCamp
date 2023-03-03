cd40c903eaa7039b3b1c00eb8465fc8e14cce234
b.chau <brianchau7979@gmail.com>
2023-03-03 11:58:58 -0600

    Setting Up Mongo Atlas

--- app.js
+++ app.js
@@ -15,11 +15,13 @@ const LocalStrategy = require("passport-local");
 const User = require("./models/user");
 const mongoSanitize = require("express-mongo-sanitize");
 const helmet = require("helmet");
+const dbUrl = process.env.DB_URL;
 
 const userRoutes = require("./routes/users");
 const campgroundRoutes = require("./routes/campgrounds");
 const reviewRoutes = require("./routes/reviews");
 
+// "mongodb://localhost:27017/yelp-camp"
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
